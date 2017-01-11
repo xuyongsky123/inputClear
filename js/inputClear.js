60805ce4b496cea3507d4be1739d29e51455e2d6
xuyongsky123 <xuyongsky123@outlook.com>
2017-01-11 17:10:21 +0800

    input框清空功能

--- js/inputClear.js
+++ js/inputClear.js
@@ -1,38 +1,66 @@
 function inputClear(ele){
-    var T = this;
-    this.container = $('.'+ele);  //input包装容器
-    this.inputEle = this.container.find('input');   //input输入框
-    this.deleteEle = this.inputEle.next(); //清空按钮
 
     //初始化样式及效果
-    this.init();
+    this.init(ele);
+
+    return document.getElementsByClassName(ele);
+
 }
 
-inputClear.prototype.init = function(){
+inputClear.prototype.init = function(ele){
+
     var T = this;
+    this.container = $('.'+ele);  //input包装容器
+    this.inputEle = this.container.children('input');   //input输入框
+    this.deleteEle = this.container.children('.deleteEle'); //清空按钮
 
     (function(){
         //处理ie9不支持栅格键点击触发onpropertychange事件
         if(navigator.appName == 'Microsoft Internet Explorer'){
             if(navigator.appVersion.match(/9./i) == '9.'){
                 T.inputEle.keyup(function(event){
+
+                    var t = $(this);
+                    var _btn = t.parent().children('.deleteEle');
+
                     if(event.keyCode == 8){
-                        ($(this).val() == '') ? $(this).next().fadeOut(100) : $(this).next().fadeIn(100);
+                        ;(t.val() == '') ? _btn.fadeOut(100) : _btn.fadeIn(100);
                     }
+
                 });
             }
         }
 
         T.inputEle.on('blur',function(){
-            $(this).next().fadeOut(300);
+
+            var _btn = $(this).parent().children('.deleteEle');
+
+            _btn.fadeOut(300);
+
         }).on('focus',function(){
-            ;($(this).val() == '') ? $(this).next().fadeOut(100) : $(this).next().fadeIn(100); 
+
+            var t = $(this);
+            var _btn = t.parent().children('.deleteEle');
+
+            ;(t.val() == '') ? _btn.fadeOut(100) : _btn.fadeIn(100);
+
         }).on('input propertychange',function(){
-            ;($(this).val() == '') ? $(this).next().fadeOut(100) : $(this).next().fadeIn(100);
+
+            var t = $(this);
+            var _btn = t.parent().children('.deleteEle');
+
+            ;(t.val() == '') ? _btn.fadeOut(100) : _btn.fadeIn(100);
+
         });
 
         T.deleteEle.on('click',function(){
-            $(this).hide().prev().val('');
+
+            var t = $(this);
+            var _btn = t.parent().children('input')
+
+            t.hide();
+            _btn.val('');
+
         });
 
     })();
